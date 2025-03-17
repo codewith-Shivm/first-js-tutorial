009badfe61081b614433e61628491593332e0b4f
codewith-Shivm <155800120+codewith-Shivm@users.noreply.github.com>
2025-03-17 13:37:00 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,8 @@
+"use strict" // treat all JS code as newer version 
+
+// alert(3 + 3) // we are using nodejs not browser in nodejs it will throw error. we have other way to use it in node js 
+
+// console.log(3 + 3); console.log("shivam") // dont do this maintain code reusability
+
+console.log(3 + 3);
+console.log("shivam")
